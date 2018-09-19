c6eaafc864f630a85a4a0e102c94039c873667d9
Paola Arredondo <arredondop03@gmail.com>
2018-09-18 20:27:15 -0400

    background

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ function AboutMe(props) {
 
   return(
     <div>
-      <h1>Who is this Jaw-dropping girl></h1>
+      <h1>Who is this Jaw-dropping girl</h1>
       <br />
       <p>  Ut amet veniam laboris deserunt et.Dolore i'
         d eu pariatur do incididunt et proident nisi dolor nostrud.
@@ -13,8 +13,6 @@ function AboutMe(props) {
         pariatur et ipsum consequat. Occaecat mollit sit id esse. 
       </p>
       <br />
-      <img href = "https://photos.app.goo.gl/D8b2YJhQbUevLHhRA" />
-
     </div>
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import  '../App.css'
 import HomePageLinks from './HomePageLinks'
 import AboutMe from './AboutMe';
 
@@ -6,13 +7,14 @@ import AboutMe from './AboutMe';
 function Home() {
   return (
     <div>
+      <div className="background-stripe-yellow" />
       <h1>Hello!</h1>
       <h2>Welcome to my portfolio</h2>
       <HomePageLinks pageName="My Work" /> <br />
       <HomePageLinks pageName="Contact Me" /><br />
       <HomePageLinks pageName="About Me" /><br />
       <HomePageLinks pageName="3 Words" /><br />
-      <AboutMe image="https://photos.app.goo.gl/D8b2YJhQbUevLHhRA"/>
+      {/* <AboutMe /> */}
 
     </div>
   )
