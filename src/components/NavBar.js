2cb937be7434cfae13eb290928348dd67cb99e10
Paola Arredondo <arredondop03@gmail.com>
2018-09-21 09:15:34 -0400

    logo and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../diamond-only.svg'
 
 function NavBar(){
   return(
-    <div>
-      <ul>
+    <div className="nav-bar">
+    <img src={logo} className="nav-bar-logo" />
+      <ul className="nav-bar-ul">
         <li>About Me</li>
         <li>My work</li>
         <li>Contact</li>
