c4ac427f44a058bf14a2452f8e04e4872d8ec917
Paola Arredondo <arredondop03@gmail.com>
2018-09-20 20:34:49 -0400

    new background and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,16 @@ import React from 'react'
 import  '../App.css'
 import HomePageLinks from './HomePageLinks'
 import AboutMe from './AboutMe';
+import NavBar from './NavBar'
 
 
 function Home() {
   return (
     <div>
-      <div className="background-stripe-yellow" />
-      <h1>Hello!</h1>
-      <h2>Welcome to my portfolio</h2>
-      <HomePageLinks pageName="My Work" /> <br />
-      <HomePageLinks pageName="Contact Me" /><br />
-      <HomePageLinks pageName="About Me" /><br />
-      <HomePageLinks pageName="3 Words" /><br />
-      {/* <AboutMe /> */}
-
+      <div className="hero">
+      <NavBar />
+        <h1> hey</h1>
+      </div>
     </div>
   )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar(){
+  return(
+    <div>
+      <ul>
+        <li>About Me</li>
+        <li>My work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default NavBar
