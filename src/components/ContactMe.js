44f9bd118f33d33877d521b75a8b5dee2e7ebe36
Paola Arredondo <arredondop03@gmail.com>
2018-10-07 23:24:16 -0400

    new icon

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,18 +4,18 @@ import React from 'react'
 function ContactMe() {
   return (
     <div className="component test3">
-    <h2>Contact Me</h2>
-      <div className='elements-container'>
+    <h2 className="contact-me-h2">Contact Me</h2>
+      <div className='elements-container contact-me-container'>
       <div className='element contact-me-element'>
-      <a href='#'> <img src="Diamond-1.png" /> </a>
+      <a className='contact-me-link' href='https://www.linkedin.com/in/paola-arredondo-acosta/'> <img src="/linkedin-gold.svg" /> </a>
       </div>
 
       <div className='element contact-me-element'>
-      <a href='#'> <img src="Diamond-1.png" /> </a>
+      <a className='contact-me-link' href='https://github.com/arredondop03'> Github </a>
       </div>
 
       <div className='element contact-me-element'>
-      <a href='#'> <img src="Diamond-1.png" /> </a>
+      <a className='contact-me-link' href='mailto:arredondop03@gmail.com'> Email </a>
       </div>
 
 
